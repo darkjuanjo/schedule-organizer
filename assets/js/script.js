0a30bc8a4ab15a3b562da59475931f1f6d40aa17
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-07-24 00:29:52 -0700

    added save feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,82 @@ var check_date = date => {
         }
     }
 }
+
+// var buttons = $("saveBtn")
+$(".saveBtn").on("click", function() {
+    let element = $(this).closest("div[id]");
+    let id = $(element).attr("id");
+    let text_area = $(element).find("textarea");
+    let task = $(element).find("textarea").val();
+    let current_button = this;
+    
+    if($(text_area).hasClass("past") == false)
+    {
+     save(id, task, current_button);
+    }
+ } );
+
+ var save = (id, task, button) => {
+    var storage = JSON.parse(localStorage.getItem("schedule")) || [];
+    var data = {
+        id: id,
+        task: task
+    };
+    try {
+        if(storage.length !== 0)
+        {
+            var position = inlist(storage, id);
+                if(position != -1)
+                {
+                    if(task !== "")
+                    {
+                    storage[position].task = task;
+                    }
+                    else {
+                        storage = removeitem(storage, position);
+                    }
+                }
+                else {
+                    if(task!== "")
+                    {
+                    storage.push(data);
+                    }
+                }
+        }
+        else{
+            if(task!= "")
+            {
+            storage.push(data);
+            }
+        }
+         localStorage.setItem("schedule", JSON.stringify(storage));
+    }
+    catch{
+        alert("Error Saving!");
+        console.log("Error Saving!");
+    }
+};
+
+var inlist = (array, id) => {
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i].id === id)
+        {
+         return i   
+        }
+    }
+    return -1
+};
+
+var removeitem = (array, position) => {
+    var new_array = [];
+    for(let i = 0; i < array.length; i++)
+    {
+        if(i !== position)
+        {
+            new_array.push(array[i]);
+        }
+    }
+
+    return new_array;
+};
